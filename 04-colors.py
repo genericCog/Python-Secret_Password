acffe570e6a362697e315759391378c6be4dd1ab
Adam Cherochak <genericCog@users.noreply.github.com>
2016-11-01 14:33:01 -0700

    complete code for project

---
+++ 04-colors.py
@@ -0,0 +1,6 @@
+favorite_color = input("What's your favorite color? ")
+
+if favorite_color == "red" and favorite_color == "blue":
+    print("That's one of my favorites too!")
+else:
+    print("If you say so...")
